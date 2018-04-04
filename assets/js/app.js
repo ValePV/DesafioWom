ac6d4e3aaab1de6b4ce10e9459ebb463e29d2d98
tamybl <tamara.bravo.l@gmail.com>
2018-04-04 11:21:11 -0300

    Ventana agregar equipos para comparar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+
   $.ajax({
   url: 'https://raw.githubusercontent.com/ValePV/DesafioWom/master/assets/js/data2.json',
   type: 'GET',
@@ -10,11 +11,13 @@ $(document).ready(() => {
 
 
 function success(data) {
+    var numMobile = 0;
+    var idEquipos = [];
     console.log(data.nextel_equipos);
-    var equipos = data.nextel_equipos;
+    const equipos = data.nextel_equipos;
     
-      for(i = 0; i< equipos.length ; i++){
-        $('.cels').append("<div class='contain-inf-cel col-md-6'><div class='row'><div class='col-md-5 contain-cel'><img src='assets/img/" + equipos[i]['imagen1'] + "'><img class='soport' src='assets/img/soporte.png'>"
+      for(i = 0; i< equipos.length ; i++) {
+        $('.cels').append("<div class='contain-inf-cel col-md-6'><div class='row'><div class='col-md-5 contain-cel'><span style='display: none;' class='id-equipo'>"+i+"</span><img src='assets/img/" + equipos[i]['imagen1'] + "' class='image_product'><img class='soport' src='assets/img/soporte.png'>"
           + "</div><div class='contain-info col-md-7'><h2>" + equipos[i]['marca_id'] + "</h2>"
           + "<h2>" + equipos[i]['nombre'] + "</h2><h4>PORTATÉ A UN PLAN Y PODRÁS:</h4>"
           + "<div class='precio'><button class='but-vert'>compra</button><span class='valor'>$" + equipos[i]['valor_venta']
@@ -23,19 +26,52 @@ function success(data) {
       }
       $('.compare').click(function(){
         if(numMobile < 2) {
-          console.log('click');
+          var title = $(this).parent().parent().find('.contain-info h2:nth-child(2)').text();
+          var position = $(this).parent().parent().find('.id-equipo').text();
+          idEquipos.push(position);
+
+          console.log(idEquipos);
+          var textNode = '<li>'+ title +'</li>';
+          console.log(title);
+          component('div', 'product', textNode, '.compare-section');
+
+          //numMobile++;
+          if(numMobile < 1) {
+             component('button', 'btn btn-compare', 'COMPARAR', '.compare-section');
+             $('.compare-section').find('.btn').prop('disabled', true);
+          }
+          else {
+            console.log('botoncito de comparar se habilita');
+            $('.compare-section').find('.btn').prop('disabled', false);
+            console.log($('.compare-section').find('.btn'));
+          }
+        }
+        if(numMobile == 2) {
           numMobile++;
+          
         }
-        else {
+        if (numMobile > 2) {
+
           alert('Haz alcanzado el maximo de dispositivos a comparar');
+
         }
-        
+        numMobile++;
       })
+
     }
 
 function error(error) {
   console.log(error);
 };
+
+function component (element, classAtr, contentNode, appendNode) {
+  var cont = document.createElement(element);
+  cont.setAttribute("class", classAtr);
+  cont.innerHTML = contentNode;
+  $(appendNode).append(cont);
+  //console.log(cont);
+}
+
 
 
 /*
